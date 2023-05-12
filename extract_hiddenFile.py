d1373ea86c30c2988767b16f89fcc6ec976d3484
xoder <xoderxoderx@gmail.com>
2023-05-12 05:12:51 -0400

    done

---
+++ extract_hiddenFile.py
@@ -0,0 +1,14 @@
+#!/bin/python
+import struct
+
+def extract():
+        f = open('flag.png', 'rb')
+        bytes = f.read()
+        #print(bytes)
+        end = bytes.find(b'\x49\x45\x4e\x44\xae\x42\x60\x82')
+        if end:
+           out = open('out', 'wb')
+           out.write( bytes[end+len(b'\x49\x45\x4e\x44\xae\x42\x60\x82'):] )
+           out.close()         
+extract()
+         
